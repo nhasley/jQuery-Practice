08a123cc155987490b3691ea68c3525993ff837e
nhasley <novahasley@gmail.com>
2019-07-31 18:21:10 -0500

    jQuery Practice

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$("#add").on("click", function() {
+  let $input = $("input").val();
+  console.log($input);
+  $(".main").append(`<div id='text'><span id='box'>X</span>${$input}</div>`);
+
+  $("input").val("");
+});
+
+$(".main").on("click", function() {
+  let $listtext = $("#text");
+  $listtext.remove();
+});
